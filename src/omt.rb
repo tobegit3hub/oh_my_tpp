c8f5246e0f626752a0f86596b9f104b447930a1f
tobe <tobegit3hub@gmail.com>
2013-12-22 22:36:24 +0800

    add class page for each page

--- src/omt.rb
+++ src/omt.rb
@@ -1,19 +1,39 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 
+class Page
+  attr_accessor :contents
+
+  def initialize
+    @contents = {}
+  end
+  
+  def title(title)
+    @contents[title] = :title
+  end
+
+  def text(content)
+    @contents[content] = :text
+  end
+
+end
+
+
 class OhMyTpp
 
-  def initialized
-    @pages
+  def initialize
+    @pages = []
   end
 
   def load(file_name)
     self.instance_eval(File.read(file_name))
   end
-
   
   def page(title="title")
     puts "title = #{title}"
+    page = Page.new
+    page.title title
+    @pages << page
     yield if block_given?
   end
 
@@ -41,8 +61,8 @@ end
 
 
 if __FILE__ == $0
+  omt_file_name = ARGV.first
   omt = OhMyTpp.new
-  file_name = ARGV.first
-  omt.load file_name
+  omt.load omt_file_name
 end
 
